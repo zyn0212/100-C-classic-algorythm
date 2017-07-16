a1c78915b34246c5beff9ff2151e5d3a63693efc
user <user@usermatoMacBook-Pro.local>
2017-07-16 20:00:39 +0800

    complete Algorythm 001

---
+++ 001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int i = 0, j = 0, k = 0, count = 0;
+	for( i = 1; i < 5; ++i )
+		for( j = 1; j < 5; ++j )
+			for( k = 1; k < 5; ++k )
+				if( i != j && i != k && j != k )
+				{
+					printf("%d %d %d\n", i, j, k);
+					++count;
+				}
+	printf("total = %d\n", count);
+	return 0;	
+}
