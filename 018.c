a7231f836b2bf4f25e986881b5c333043695118f
zhaoyining <zyn0212@126.com>
2017-07-18 01:42:00 +0800

    advance 018 first

--- 018.c
+++ 018.c
@@ -3,7 +3,7 @@
 #define MAX 400
 int main(void)
 {
-//	char result[MAX];
+	char result[MAX];
 	int n = 0, i = 0, num = 0;
 	long int sum = 0, base = 0;
 	scanf("%d,%d", &n, &num);
@@ -11,14 +11,9 @@ int main(void)
 	{
 		base = base * 10 + 1;
 		sum += base;
-		//sprintf(result, "%s %ld +", result, base * num); 
+		sprintf(result, "%s + %ld", result, base * num); 
 	}
-	printf("%ld = ", sum * num);
-	for( i = 0, base = 0; i < n - 1; ++i )
-	{
-		base = base * 10 + 1;
-		printf(" %ld +", base * num);
-	}
+	printf("%ld = %d %s", sum * num, num, result);
 	printf(" %ld\n", (base * 10 + 1) * num);
 	return 0;
 }
