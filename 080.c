3bbf1cd0301a86333a54504a27d3bb0688082813
zhaoyining <zyn0212@126.com>
2017-07-24 08:59:48 +0800

    recorrected 080

--- 080.c
+++ 080.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+static void GetResult(int n, int base, int result);
 int main(void)
 {
-	int n = 0, result = 6;
-	scanf("%d", &n);
-	while( n-- > 1 )
-		result = result * 5 + 1;
-	printf("there are %d pinks!\n", result);
+	GetResult(5, 1, 4);
 	return 0;
+}
+static void GetResult(int n, int base, int result)
+{
+	if( 0 == n )
+		printf("there are %d peachs on the beach!\n", result);
+	else
+	{
+		while( 0 != (result & 3) )
+			result += base;
+		GetResult(n - 1, base * 5, (result >> 2) * 5 + 1);
+	}
 }
