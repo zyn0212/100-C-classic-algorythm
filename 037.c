918f292c5d4fba8249f24cdc024a43fd0c8afc0c
zhaoyining <zyn0212@126.com>
2017-07-24 09:19:25 +0800

    completed 037

---
+++ 037.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define MAX 10
+int main(void)
+{
+	srand(time(0));
+	int i = 0, j = 0, array[MAX];
+	for( i = 0; i < MAX; ++i )
+		printf("%d\t", array[i] = rand() % 100 + 1);
+	printf("\n");
+	for( i = 1; i < MAX; ++i )
+		for( j = MAX - 1; j >= i; --j )
+			if( array[j] < array[j - 1] )
+				array[j] ^= array[j - 1] ^= array[j] ^= array[j - 1];
+	for( i = 0; i < MAX; ++i )
+		printf("%d\t", array[i]);
+	printf("\n");
+	return 0;
+}
