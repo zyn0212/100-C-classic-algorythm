b435e1839556365abe83b1c52c1114e16da95b7e
zhaoyining <zyn0212@126.com>
2017-11-01 12:28:07 +0800

    completed 097

---
+++ 097.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define PATHMAX 50
+#include <string.h>
+int main(int argc, char *argv[])
+{
+	if( 2 != argc ) {
+		printf("wrong parameters!\n");
+		return 1;
+	}
+	char path[PATHMAX];
+	memset(path, 0, sizeof path);
+	FILE *fp = NULL;
+	sprintf(path, "%s%s", '/' == *argv[1] ? "" : "./", argv[1]);
+	if( NULL == (fp = fopen(path, "w")) ) {
+		printf("open file %s failed!\n", path);
+		return 2;
+	}
+	int c = 0;
+	while( '#' != (c = getchar()) && EOF != c )
+		fputc(c, fp);
+	fclose(fp);
+	fp = NULL;
+	return 0;
+}
